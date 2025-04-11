e29a187c65adbc71817d7b06e93a50741a59473a
MerelDej <merel.dejaegere@student.vives.be>
2025-04-11 15:53:17 +0200

    Information and finalfantasy cards added

--- PractiseEnterprise/Controllers/HomeController.cs
+++ PractiseEnterprise/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace PractiseEnterprise.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Soon()
         {
             return View();
         }
